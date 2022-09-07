68808c289f4362e1cfdd83564d9e3ec272e7bd24
Miguel Ángel Rapado <87069757+MglAnglRT@users.noreply.github.com>
2022-09-07 20:15:42 +0200

    temp

--- Fortran/sources/Foundations/Basic operations/Basic_operations.f90
+++ Fortran/sources/Foundations/Basic operations/Basic_operations.f90
@@ -14,26 +14,6 @@ contains
          
  end subroutine 
  
- subroutine Data_structures()
- 
-    integer, parameter :: N = 10 
-    real :: W(N), V(N), A(N,N), X(6), Y(3)
-    integer :: i, j  
-    real :: C(5,4), B(-2:4,0:1)
-
-    V = [ ( 1./i**2, i=1, N ) ] 
-    W = [ ( (-1)**(i+1)/(2*i+1.), i=1, N ) ] 
-    A = reshape( [ ( ( (i/real(N))**(j-1), i=1, N ),  j=1, N) ], [N, N] )  
-    X = [ 1.3, 2.4, 3., 4.5, 5.3, 7. ]
-    Y = [ A(2, 3:5) ]
-    
-    !Slices
-    C(:,2) = X(1:5)
-    C(2:3,3:4) = reshape( [1., 2., 3., 4.], [2,2] ) !enter by columns!
-    B(-2:4:3,:) = reshape( [1., 2., 3., 4., 5., 6.], [3,2] )
-         
- end subroutine 
- 
  subroutine Operators()
  
      real :: x = 2., y = 7.
@@ -45,7 +25,7 @@ contains
          
  end subroutine 
  
-  subroutine Flow_structures()
+ subroutine Flow_structures()
  
      integer :: n = 6, i, f, a, b
      real :: x1, x0, x, abso
@@ -90,5 +70,29 @@ contains
      end if
          
  end subroutine 
+ 
+ 
+ 
+ 
+ subroutine Data_structures()
+ 
+    integer, parameter :: N = 10 
+    real :: V(N), A(N,N), X(6), Y(3), Z(2,3)
+    integer :: i, j  
+    real :: C(5,4), B(-2:4,0:1)
+
+    V = [ ( 1./i**2, i=1, N ) ]
+    A = reshape( [ ( ( (i/real(N))**(j-1), i=1, N ),  j=1, N) ], [N, N] )  
+    X = [ 1.3, 2.4, 3., 4.5, 5.3, 7. ]
+    Y = [ A(2, 3:5) ]
+    Z = reshape( [ 1.1, 4., 2.2, 5.6, 3.3, 6.2 ], [2,3] )
+    
+    !Slices
+    C(:,2) = X(1:5)
+    C(2:3,3:4) = reshape( [1., 2., 3., 4.], [2,2] ) !enter by columns!
+    B(-2:4:3,:) = reshape( [1., 2., 3., 4., 5., 6.], [3,2] )
+         
+ end subroutine 
+
  
  end module 
